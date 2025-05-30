dde8226dbd421f39b638bec30a4470a927196b9e
Ernest Zhang <ernest.w.zhang@gmail.com>
2025-05-29 21:17:24 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,13 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Driver {
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException{
 			Polynomial p = new Polynomial();
 			System.out.println(p.evaluate(3));
-			double [] c1 = {6,0,0,5};
-			Polynomial p1 = new Polynomial(c1);
-			double [] c2 = {0,-2,0,0,-9};
-			Polynomial p2 = new Polynomial(c2);
+			double [] c1 = {3,-1,2,4};
+			int [] e1 = {0,1,2,3};
+			Polynomial p1 = new Polynomial(c1,e1);
+			double [] c2 = {1, 2, 3};
+			int [] e2 = {0,1,5};
+			Polynomial p2 = new Polynomial(c2, e2);
 			Polynomial s = p1.add(p2);
+			Polynomial d = p1.multiply(p2);
+			System.out.println(d);
 			System.out.println("s(0.1) = " + s.evaluate(0.1));
+			
+			File poly = new File("polyfile.txt");
+			Polynomial p3 = new Polynomial(poly);
+			System.out.println(p3);
+			p3.saveToFile("polyfile2");
+			
 			if(s.hasRoot(1))
 				System.out.println("1 is a root of s");
 			else
--- Polynomial.java
+++ Polynomial.java
@@ -1,33 +1,140 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Scanner;
+import java.io.*;
 
 public class Polynomial {
-	double[] coeffs;
+	double[] coefficients;
+	int[] exponents;
 	
 	public Polynomial() {
-		coeffs = new double[] {0};
+		coefficients = new double[] {0};
+		exponents = new int[] {0};
 	}
 	
-	public Polynomial(double[] coeffs) {
-		this.coeffs = Arrays.copyOf(coeffs, coeffs.length);
+	public Polynomial(double[] coefficients, int[] exponents) {
+		this.coefficients = Arrays.copyOf(coefficients, coefficients.length);
+		this.exponents = Arrays.copyOf(exponents, exponents.length);
 	}
 	
+	public Polynomial(File f) {
+		try {
+			Scanner scan = new Scanner(f);
+			String a = scan.nextLine();
+			scan.close();
+			int start = 0;
+			ArrayList <String> list = new ArrayList <String>();
+			for(int i = 0; i<a.length(); i++) {
+				if((a.charAt(i) == '-' || a.charAt(i) == '+') && i != 0) {
+//					System.out.println(a.substring(start,i));
+					list.add(a.substring(start,i));
+					start = i;
+				}
+				if(i == a.length()-1) {
+//					System.out.println(a.substring(start));
+					list.add(a.substring(start));
+				}
+			}
+			double[] coeff = new double[list.size()];
+			int[] exponents = new int[list.size()];
+			
+			for(int i = 0; i<list.size(); i++) {
+				String[] b = list.get(i).split("x");
+				coeff[i] = Double.parseDouble(b[0]);
+				if(b.length == 2) {
+					exponents[i] = Integer.parseInt(b[1]);
+				}
+			}
+			this.coefficients = Arrays.copyOf(coeff, coeff.length);
+			this.exponents = Arrays.copyOf(exponents,  exponents.length);
+		}catch (FileNotFoundException ex) {
+			System.out.println("No file found");
+		}
+	}
+		
 	public Polynomial add(Polynomial p) {
-		int len1 = coeffs.length;
-		int len2 = p.coeffs.length;
-		int len = Math.max(len1, len2);
-		double[] new_coeff = Arrays.copyOf(coeffs, len);
+		int len1 = exponents.length;
+		int len2 = p.exponents.length;
+//		double[] new_coeff = new double[len];
+//		int[] new_exponents = new int[len];
+		HashMap <Integer, Double> pairs = new HashMap <Integer, Double>();
+		for(int i = 0; i<len1;i++) {
+			pairs.put(exponents[i], coefficients[i]);
+		}
 		for(int i = 0; i<len2; i++) {
-			new_coeff[i] += p.coeffs[i];
+			if(pairs.get(p.exponents[i]) == null) {
+				pairs.put(p.exponents[i], p.coefficients[i]);
+			}
+			else {
+				pairs.put(p.exponents[i], pairs.get(p.exponents[i])+p.coefficients[i]);
+			}
+		}
+		Set <Integer> exponents_set = pairs.keySet();
+		double[] new_coeff = new double[exponents_set.size()];
+		int[] new_exponents = new int[exponents_set.size()];
+		int i = 0;
+		for(int h:exponents_set) {
+			new_exponents[i] = h;
+			new_coeff[i] = pairs.get(h);
+			i++;
 		}
 		
-		Polynomial q = new Polynomial(new_coeff);
+		Polynomial q = new Polynomial(new_coeff, new_exponents);
+		return q;
+	}
+	
+	public void saveToFile(String s) throws IOException{
+		//System.out.println("AAAAAAAAAAAAAAHHHHHHH I HATE FILEWRITER");
+		String poly = "";
+		for(int i = 0; i<coefficients.length; i++) {
+			if(coefficients[i]>0 && i != 0) {
+				poly += "+";
+			}
+			poly += coefficients[i];
+			if(exponents[i] != 0) {
+				poly += ("x"+exponents[i]);
+			}
+		}
+		String filename = s+".txt";
+		FileWriter writer = new FileWriter(filename);
+		writer.write(poly);
+		writer.close();
+	}
+	
+	public Polynomial multiply(Polynomial p){
+		int len1 = exponents.length;
+		int len2 = p.exponents.length;
+		HashMap <Integer, Double> pairs = new HashMap <Integer, Double>();
+		for(int i = 0; i<len1; i++) {
+			for(int j = 0; j<len2; j++) {
+				if(pairs.get(exponents[i]+p.exponents[j]) == null) {
+					pairs.put(exponents[i]+p.exponents[j],coefficients[i]*p.coefficients[j]);
+				}
+				else {
+					pairs.put(exponents[i]+p.exponents[j], pairs.get(exponents[i]+p.exponents[j])+coefficients[i]*p.coefficients[j]);
+				}
+			}
+		}
+		Set <Integer> keySet = pairs.keySet();
+		double[] new_coeff = new double[keySet.size()];
+		int[] new_exp = new int[keySet.size()];
+		int i = 0;
+		for(int h:keySet) {
+			new_exp[i] = h;
+			new_coeff[i] = pairs.get(h);
+			i++;
+		}
+		
+		Polynomial q = new Polynomial(new_coeff, new_exp);
 		return q;
 	}
 	
 	public double evaluate(double num) {
 		double a = 0;
-		for(int i = 0; i<coeffs.length; i++) {
-			a += coeffs[i]*(Math.pow(num, i));
+		for(int i = 0; i<exponents.length; i++) {
+			a += coefficients[i]*(Math.pow(num, exponents[i]));
 		}
 		return a;
 	}
@@ -38,4 +145,18 @@ public class Polynomial {
 		}
 		return false;
 	}
+	
+//	@Override 
+//	public String toString() {
+//		String s = "";
+//		for(int i = 0; i<exponents.length; i++) {
+//			if(coefficients[i] > 0) {
+//				s += "+"+coefficients[i]+"x^"+exponents[i]+" ";
+//			}
+//			else if(coefficients[i]<0) {
+//				s += coefficients[i]+"x^"+exponents[i]+" ";
+//			}
+//		}
+//		return s;
+//	}
 }
